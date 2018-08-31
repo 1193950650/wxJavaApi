699ddc5cd367a13c45d990d97ee80a52acee1b57
471002385@qq.com <471002385@qq.com>
2018-08-31 16:38:13 +0800

    新增业务

--- src/main/java/com/julu/appApi/Content_commentController.java
+++ src/main/java/com/julu/appApi/Content_commentController.java
@@ -16,7 +16,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
