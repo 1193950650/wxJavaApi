b15a0d803523466a7d7ee0d17dae95695ab63508
mhs <471002385@qq.com>
2018-09-19 23:39:31 +0800

    点赞

---
+++ src/main/java/com/julu/service/IContent_imgtext_dzlogService.java
@@ -0,0 +1,16 @@
+package com.julu.service;
+
+import com.julu.entity.Content_imgtext_dzlog;
+import com.baomidou.mybatisplus.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author mhs
+ * @since 2018-09-19
+ */
+public interface IContent_imgtext_dzlogService extends IService<Content_imgtext_dzlog> {
+
+}
